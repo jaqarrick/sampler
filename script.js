e3bb7af9d27f40b97b49880b37dc204ad9b25289
jaqarrick <pigmaask@gmail.com>
2020-10-07 16:29:31 -0700

    updated regions plugin

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 import WaveSurfer from "wavesurfer.js"
 import sample from "./ursula.mp3"
-
+import RegionsPlugin from "./node_modules/wavesurfer.js/dist/plugin/wavesurfer.regions"
 const waveSurfer = WaveSurfer.create({
   container: "#wavesurfer-container",
   waveColor: "violet",
   progressColor: "purple",
+  plugins: [
+    RegionsPlugin.create({
+      regionsMinLength: 1,
+      regions: [
+        {
+          start: 1,
+          end: 9,
+          loop: true,
+          color: "hsla(400, 100%, 30%, 0.5)",
+        },
+      ],
+      dragSelection: {
+        slop: 1,
+      },
+    }),
+  ],
 })
 
 const playBtn = document.querySelector("#play")
